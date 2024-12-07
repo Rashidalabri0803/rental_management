7d7d837fec6c4a8868baf22073de69e1bfb4ffae
Rashidalabri0803 <rashid92759346@gmail.com>
2024-12-07 14:58:52 +0400

    admin

--- rental/admin.py
+++ rental/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from .models import Unit, Tenant, Lease, Payment, MaintenanceRequest
 
-admin.site.register(Unit)
+@admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
-    list_display = ('unit_number', 'unit_type', 'floor_number', 'rent_price', 'status', 'is_available')
+    list_display = ('unit_number', 'unit_type', 'floor_number', 'rent_price', 'status')
     list_filter = ('unit_type', 'status', 'floor_number')
-    search_fields = ('unit_number', 'desription')
+    search_fields = ('unit_number', 'description')
     list_editable = ('status', 'rent_price')
     ordering = ('unit_number',)
 
     fieldsets = (
         ('المعلومات الأساسية',{
-            'fields': ('unit_number', 'unit_type', 'floor_number', 'size', 'desription'),
+            'fields': ('unit_number', 'unit_type', 'floor_number', 'size', 'description'),
         }),
         ('المعلومات المالية',{
-            'fields': ('rent_price', 'status', 'is_available'),
+            'fields': ('rent_price', 'status'),
         }),
         ('التواريخ',{
             'fields': ('created_at', 'updated_at'),
@@ -22,7 +22,6 @@ class UnitAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('created_at', 'updated_at')
 
-admin.site.register(Unit, UnitAdmin)
 admin.site.register(Tenant)
 admin.site.register(Lease)
 admin.site.register(Payment)
