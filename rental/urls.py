ee0a41f721de0f74ad6ba12b8392d6fa3a9a10f1
Rashidalabri0803 <rashid92759346@gmail.com>
2024-12-07 15:54:27 +0000

    urls.py

--- rental/urls.py
+++ rental/urls.py
@@ -1,27 +1,27 @@
 from django.urls import path
-from . import views
+from .views import UnitListView, UnitCreateView, UnitUpdateView, UnitDeleteView, UnitDetailView, LeaseListView, LeaseCreateView, LeaseUpdateView, LeaseDeleteView, PaymentListView, PaymentCreateView, MaintenanceRequestListView, MaintenanceRequestCreateView, MaintenanceRequestUpdateView
 
 urlpatterns = [
-    path('units/', views.unit_list, name='unit_list'),
-    path('units/add/', views.add_unit, name='add_unit'),
-    path('units/<int:pk>/', views.unit_detail, name='unit_detail'),
-    path('units/<int:pk>/edit/', views.edit_unit, name='edit_unit'),
-    path('units/<int:pk>/delete/', views.delete_unit, name='delete_unit'),
+    path('units/', UnitListView.as_view(), name='unit_list'),
+    path('units/<int:pk>/', UnitDetailView.as_view(), name='unit_detail'),
+    path('units/add/', UnitCreateView.as_view(), name='add_unit'),
+    path('units/<int:pk>/edit/', UnitUpdateView.as_view(), name='edit_unit'),
+    path('units/<int:pk>/delete/', UnitDeleteView.as_view(), name='delete_unit'),
 
-    path('tenants/', views.tenant_list, name='tenant_list'),
-    path('tenants/add/', views.add_tenant, name='add_tenant'),
-    path('tenants/<int:pk>/edit/', views.edit_tenant, name='edit_tenant'),
-    path('tenants/<int:pk>/delete/', views.delete_tenant, name='delete_tenant'),
+    path('tenants/', LeaseListView.as_view(), name='lease_list'),
+    path('tenants/add/', LeaseCreateView.as_view(), name='add_lease'),
+    path('tenants/<int:pk>/edit/', LeaseUpdateView.as_view(), name='edit_lease'),
+    path('tenants/<int:pk>/delete/', LeaseDeleteView.as_view(), name='delete_lease'),
 
-    path('leases/', views.lease_list, name='lease_list'),
-    path('leases/add/', views.add_lease, name='add_lease'),
-    path('leases/<int:pk>/edit/', views.edit_lease, name='edit_lease'),
-    path('leases/<int:pk>/delete/', views.delete_lease, name='delete_lease'),
+    path('leases/', LeaseListView.as_view(), name='lease_list'),
+    path('leases/add/', LeaseCreateView.as_view(), name='add_lease'),
+    path('leases/<int:pk>/edit/', LeaseUpdateView.as_view(), name='edit_lease'),
+    path('leases/<int:pk>/delete/', LeaseDeleteView.as_view(), name='delete_lease'),
 
-    path('payments/', views.payment_list, name='payment_list'),
-    path('payments/add/', views.add_payment, name='add_payment'),
+    path('payments/', PaymentListView.as_view(), name='payment_list'),
+    path('payments/add/', PaymentCreateView.as_view(), name='add_payment'),
 
-    path('maintenancers/', views.maintenance_request_list, name='maintenancer_list'),
-    path('maintenancers/add/', views.add_maintenance_request, name='add_maintenance_request'),
-    path('maintenancers/<int:pk>/edit/', views.edit_maintenance_request, name='edit_maintenance_request'),
+    path('maintenance_requests/', MaintenanceRequestListView.as_view(), name='maintenance_request_list'),
+    path('maintenance_requests/add/', MaintenanceRequestCreateView.as_view(), name='add_maintenance_request'),
+    path('maintenance_requests/<int:pk>/edit/', MaintenanceRequestUpdateView.as_view(), name='edit_maintenance_request'),
 ]
